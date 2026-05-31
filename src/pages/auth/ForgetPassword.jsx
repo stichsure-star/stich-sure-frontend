d2b9585ac5d14e993139a776391a787895dee4fd
AderemiGbenga <gbengaaderemi824@gmail.com>
2026-05-31 23:12:14 +0100

    Beginning of stitchy

---
+++ src/pages/auth/ForgetPassword.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ForgetPassword = () => {
+  return (
+    <div>
+      <h3>Hello</h3>
+    </div>
+  );
+};
+
+export default ForgetPassword;
---
+++ src/pages/auth/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <h3>Hello</h3>
+    </div>
+  );
+};
+
+export default Signup;
